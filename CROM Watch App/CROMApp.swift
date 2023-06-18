7e24e5c5e89f557513d0b601c12c7bb1df1e92a2
allanhull <allanpaulhull@outlook.com>
2023-06-17 23:33:19 -0500

    Initial Commit

---
+++ CROM Watch App/CROMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CROMApp.swift
+//  CROM Watch App
+//
+//  Created by Allan Hull on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CROM_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CROM/CROMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CROMApp.swift
+//  CROM
+//
+//  Created by Allan Hull on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CROMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
